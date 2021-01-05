d91684dc6ec75fe297a49706d84aa704152a03b9
lisovskey <maxim.lisovsky@gismart.com>
2021-01-05 17:53:37 +0200

    refactor qa module, fix typing and codestyle

--- bi_utils/qa.py
+++ bi_utils/qa.py
@@ -1,106 +1,121 @@
 import pandas as pd
-from typing import List, Dict
+from typing import Any, Dict, Hashable, Optional, Sequence, Tuple, Union
+
 from .logger import get_logger
+
 
 logger = get_logger(__name__)
 
 
-def passert(passed: bool, message: str) -> int:
-    if passed:
-        return 0
-    elif not passed:
-        logger.warning(msg=message)
-        return 1
-    else:
-        logger.warning(msg=f"Unexpected value {passed} in passert function")
-
-
-def thresholds_test(df: pd.DataFrame(), thresholds: Dict) -> int:
-    failcount = 0
-    if thresholds is not None:
-        for c_tuple in thresholds:
-            for c in c_tuple:
-                failcount += passert(
-                    df[c].min() >= thresholds[c_tuple][0],
-                    f'Found {c} value below {thresholds[c_tuple][0]}')
-                failcount += passert(
-                    df[c].max() <= thresholds[c_tuple][1],
-                    f'Found {c} value above {thresholds[c_tuple][1]}')
-    return failcount
-
-
-def quantile_test(df: pd.DataFrame(), max_quantile: Dict) -> int:
-    failcount = 0
-    if max_quantile is not None:
-        for c in max_quantile:
-            q_threshold = df[c].quantile(max_quantile[c][0])
-            if q_threshold <= 0:
-                logger.warning(
-                    f'Skipping {c} max_quantile because threshold <= 0')
-                continue
-            failcount += passert(
-                (df[c].max() / q_threshold) <= max_quantile[c][1],
-                f'Found {c} value above {q_threshold}')
-    return failcount
-
-
-def query_test(df: pd.DataFrame(), verify_query: List) -> int:
-    failcount = 0
-    if verify_query is not None:
-        for q in verify_query:
-            n_compliant_rows = df.query(q).shape[0]
-            df_len = df.shape[0]
-            failcount += passert(
-                n_compliant_rows == df_len,
-                f'Found {df_len - n_compliant_rows} rows incompliant with {q}')
-    return failcount
-
-
-def unique_index_test(df: pd.DataFrame(), unique_index: List) -> int:
-    failcount = 0
-    if unique_index is not None:
-        failcount += passert(
-            ~df.duplicated(subset=unique_index, keep='first').any(),
-            'Found dups')
-    return failcount
-
-
 def df_test(
-    df: pd.DataFrame(),
+    df: pd.DataFrame,
     strict: bool = False,
-    nullable_cols: List = [],
-    unique_index: List or None = None,
-    thresholds: Dict[tuple, list] or None = None,
-    max_quantile: Dict or None = None,
-    verify_query: List[str] or None = None,
+    nullable_cols: Optional[Sequence[Hashable]] = None,
+    unique_index: Optional[Sequence[Hashable]] = None,
+    thresholds: Optional[Dict[Any, Tuple[Union[int, float], Union[int, float]]]] = None,
+    max_quantiles: Optional[Dict[Any, Tuple[float, int]]] = None,
+    verify_queries: Optional[Sequence[str]] = None,
 ) -> int:
-    '''Check dataframe for emptiness, check non-nullable_cols columns for NAs, run quantile and
+    '''
+    Check dataframe for emptiness, check non-nullable_cols columns for NAs, run quantile and
     threshold tests for numeric data, verify that all rows comply with custom requirements (queries)
 
     Keyword arguments:
     nullable_cols -- columns allowed to contain NAs
     unique_index -- list of columns implied to be a unique row identifier
     thresholds -- columns with numeric values that should be within the specified [min, max] range
-    max_quantile -- dict of positive numeric columns {column_name: [Q, K]} that should not
+    max_quantiles -- dict of positive numeric columns {column_name: [Q, K]} that should not
                          contain values that are K+ times greater then quantile Q (float)
-    verify_query -- list of positive requirements (queries) all rows should comply with
+    verify_queries -- list of positive requirements (queries) all rows should comply with
     '''
 
     failcount = 0
     if df.empty:
-        raise ValueError('The dataframe is empty.')
+        raise ValueError('The dataframe is empty')
     cols = df.columns
 
+    if nullable_cols is None:
+        nullable_cols = []
     not_nullable_cols = [c for c in cols if c not in nullable_cols]
     nans = df[not_nullable_cols].isna().sum()
-    failcount += passert(nans.sum() == 0,
-                         f'Found NA in columns: {nans[nans>0]}')
+    failcount += _passert(
+        nans.sum() == 0,
+        f'Found NA in columns: {nans[nans > 0]}',
+    )
 
     failcount += thresholds_test(df, thresholds)
-    failcount += quantile_test(df, max_quantile)
-    failcount += query_test(df, verify_query)
+    failcount += quantile_test(df, max_quantiles)
+    failcount += query_test(df, verify_queries)
     failcount += unique_index_test(df, unique_index)
     if strict and failcount > 0:
-        raise ValueError(
-            f'Data qa failcount: {failcount}, force exit since in strict mode')
+        raise ValueError(f'Data qa failcount: {failcount}, force exit since in strict mode')
     return failcount
+
+
+def thresholds_test(
+    df: pd.DataFrame,
+    thresholds: Optional[Dict[Hashable, Tuple[Union[int, float], Union[int, float]]]],
+) -> int:
+    failcount = 0
+    if thresholds is None:
+        thresholds = {}
+    for col, (min_threshold, max_threshold) in thresholds.items():
+        failcount += _passert(
+            df[col].min() >= min_threshold,
+            f'Found {col} value below {min_threshold}',
+        )
+        failcount += _passert(
+            df[col].max() <= max_threshold,
+            f'Found {col} value above {max_threshold}',
+        )
+    return failcount
+
+
+def quantile_test(
+    df: pd.DataFrame,
+    max_quantiles: Optional[Dict[Hashable, Tuple[float, int]]],
+) -> int:
+    failcount = 0
+    if max_quantiles is None:
+        max_quantiles = {}
+    for col, (quantile, max_multiplier) in max_quantiles.items():
+        q_threshold = df[col].quantile(quantile)
+        if q_threshold <= 0:
+            logger.warning(f'Skipping {col} max_quantiles because threshold <= 0')
+            continue
+        failcount += _passert(
+            (df[col].max() / q_threshold) <= max_multiplier,
+            f'Found {col} value above {q_threshold}',
+        )
+    return failcount
+
+
+def query_test(df: pd.DataFrame, verify_queries: Optional[Sequence[str]]) -> int:
+    failcount = 0
+    if verify_queries is None:
+        verify_queries = []
+    for q in verify_queries:
+        n_compliant_rows = df.query(q).shape[0]
+        df_len = df.shape[0]
+        failcount += _passert(
+            n_compliant_rows == df_len,
+            f'Found {df_len - n_compliant_rows} rows incompliant with {q}',
+        )
+    return failcount
+
+
+def unique_index_test(df: pd.DataFrame, unique_index: Optional[Sequence[Hashable]]) -> int:
+    failcount = 0
+    if unique_index is not None:
+        failcount += _passert(
+            ~df.duplicated(subset=unique_index, keep='first').any(),
+            'Found duplicates',
+        )
+    return failcount
+
+
+def _passert(passed: bool, message: str) -> int:
+    if passed:
+        return 0
+    logger.warning(message)
+    return 1
--- tests/test_qa.py
+++ tests/test_qa.py
@@ -1,16 +1,16 @@
 import pytest
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from bi_utils import qa
 
 
 def test_passert_negative():
-    assert qa.passert(False, 'failed') == 1
+    assert qa._passert(False, 'failed') == 1
 
 
 def test_passert_positive():
-    assert qa.passert(True, 'passed') == 0
+    assert qa._passert(True, 'passed') == 0
 
 
 @pytest.mark.parametrize('df', [pd.DataFrame(), pd.DataFrame({1: [], 2: []})])
@@ -19,100 +19,155 @@ def test_empty_df_positive(df):
         qa.df_test(df)
 
 
-def test_empty_df_negative(df=pd.DataFrame({1: [3, 4], 2: [1, 2]})):
-    assert qa.df_test(df) == 0
+def test_empty_df_negative(sample_data):
+    assert qa.df_test(sample_data, nullable_cols=[1]) == 0
 
 
-@pytest.mark.parametrize('df, unique_index, exp_status', [
-    (pd.DataFrame({
-        'country': ['US', 'US', 'CN', 'CN'],
-        'payment_number': [1, 2, 3, 1]
-    }), ['country', 'payment_number'], 0),
-    (pd.DataFrame({
-        'country': ['US', 'US', 'CN', 'CN'],
-        'payment_number': [1, 2, 3, 3]
-    }), ['country', 'payment_number'], 1),
-    (pd.DataFrame({
-        'country': ['US', 'US', 'CN', 'CN'],
-        'payment_number': [1, 2, 3, 1]
-    }), ['payment_number'], 1),
-    (pd.DataFrame({
-        'country': ['US', 'US', 'CN', 'CN'],
-        'payment_number': [1, 2, 3, np.nan]
-    }), ['payment_number'], 0),
-])
+@pytest.mark.parametrize(
+    'df,unique_index,exp_status',
+    [
+        (
+            pd.DataFrame({
+                'country': ['US', 'US', 'CN', 'CN'],
+                'payment_number': [1, 2, 3, 1],
+            }),
+            ['country', 'payment_number'],
+            0,
+        ),
+        (
+            pd.DataFrame({
+                'country': ['US', 'US', 'CN', 'CN'],
+                'payment_number': [1, 2, 3, 3],
+            }),
+            ['country', 'payment_number'],
+            1,
+        ),
+        (
+            pd.DataFrame({
+                'country': ['US', 'US', 'CN', 'CN'],
+                'payment_number': [1, 2, 3, 1],
+            }),
+            ['payment_number'],
+            1,
+        ),
+        (
+            pd.DataFrame({
+                'country': ['US', 'US', 'CN', 'CN'],
+                'payment_number': [1, 2, 3, np.nan],
+            }),
+            ['payment_number'],
+            0,
+        ),
+    ]
+)
 def test_unique(df, unique_index, exp_status):
     assert qa.unique_index_test(df, unique_index) == exp_status
 
 
-@pytest.mark.parametrize('df, thresholds, exp_status', [
-    (pd.DataFrame({
-        'conv_1': [0.5, 1.01],
-        'conv_2': [0.001, 0.999]
-    }), {
-        ('conv_1', ): [0, 1]
-    }, 1),
-    (pd.DataFrame({
-        'conv_1': [0.5, 2],
-        'conv_2': [0.001, 0.999]
-    }), {
-        ('conv_2', ): [0, 1]
-    }, 0),
-])
+@pytest.mark.parametrize(
+    'df,thresholds,exp_status',
+    [
+        (
+            pd.DataFrame({
+                'conv_1': [0.5, 1.01],
+                'conv_2': [0.001, 0.999],
+            }),
+            {'conv_1': [0, 1]},
+            1,
+        ),
+        (
+            pd.DataFrame({
+                'conv_1': [0.5, 2],
+                'conv_2': [0.001, 0.999],
+            }),
+            {'conv_2': [0, 1]},
+            0,
+        ),
+    ]
+)
 def test_thresholds(df, thresholds, exp_status):
     assert qa.thresholds_test(df, thresholds) == exp_status
 
 
-@pytest.mark.parametrize('df, q, exp_status', [
-    (pd.DataFrame({
-        'ltv_180': [0.5, 1.01, 0.75, 8.99, 70],
-        'ltv_365': [0.5, 1.01, 1.3, 12.3, 70]
-    }), {
-        'ltv_180': (0.75, 2)
-    }, 1),
-    (pd.DataFrame({
-        'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
-        'ltv_365': [0.5, 1.01, 1.3, 12.3, 70]
-    }), {
-        'ltv_180': (0.75, 2)
-    }, 0),
-    (pd.DataFrame({
-        'ltv_180': [-0.5, 1.01, 0.75, 0.76, 1.3],
-        'ltv_365': [0.5, 1.01, 1.3, 12.3, 70]
-    }), {
-        'ltv_180': (0.75, 2)
-    }, 0),
-])
+@pytest.mark.parametrize(
+    'df,q,exp_status',
+    [
+        (
+            pd.DataFrame({
+                'ltv_180': [0.5, 1.01, 0.75, 8.99, 70],
+                'ltv_365': [0.5, 1.01, 1.3, 12.3, 70],
+            }),
+            {'ltv_180': (0.75, 2)},
+            1,
+        ),
+        (
+            pd.DataFrame({
+                'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
+                'ltv_365': [0.5, 1.01, 1.3, 12.3, 70],
+            }),
+            {'ltv_180': (0.75, 2)},
+            0,
+        ),
+        (
+            pd.DataFrame({
+                'ltv_180': [-0.5, 1.01, 0.75, 0.76, 1.3],
+                'ltv_365': [0.5, 1.01, 1.3, 12.3, 70],
+            }),
+            {'ltv_180': (0.75, 2)},
+            0,
+        ),
+    ]
+)
 def test_quantiles(df, q, exp_status):
     assert qa.quantile_test(df, q) == exp_status
 
 
-@pytest.mark.parametrize('df, q, exp_status',
-                         [(pd.DataFrame({
-                             'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
-                             'ltv_365': [0.5, 1, 1.3, 12.3, 70]
-                         }), ['ltv_365 >= ltv_180'], 1),
-                          (pd.DataFrame({
-                              'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
-                              'ltv_365': [0.5, 1.01, 1.3, 12.3, 70]
-                          }), ['ltv_365 > ltv_180'], 1),
-                          (pd.DataFrame({
-                              'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
-                              'ltv_365': [0.5, 1.01, 1.3, 12.3, 70]
-                          }), ['ltv_365 >= ltv_180'], 0),
-                          (pd.DataFrame({
-                              'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
-                              'ltv_365': [0.5, 1.01, 1.3, 12.3, np.nan]
-                          }), ['ltv_365 >= ltv_180'], 1)])
+@pytest.mark.parametrize(
+    'df,q,exp_status',
+    [
+        (
+            pd.DataFrame({
+                'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
+                'ltv_365': [0.5, 1, 1.3, 12.3, 70],
+            }),
+            ['ltv_365 >= ltv_180'],
+            1,
+        ),
+        (
+            pd.DataFrame({
+                'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
+                'ltv_365': [0.5, 1.01, 1.3, 12.3, 70],
+            }),
+            ['ltv_365 > ltv_180'],
+            1,
+        ),
+        (
+            pd.DataFrame({
+                'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
+                'ltv_365': [0.5, 1.01, 1.3, 12.3, 70],
+            }),
+            ['ltv_365 >= ltv_180'],
+            0,
+        ),
+        (
+            pd.DataFrame({
+                'ltv_180': [0.5, 1.01, 0.75, 0.76, 1.3],
+                'ltv_365': [0.5, 1.01, 1.3, 12.3, np.nan],
+            }),
+            ['ltv_365 >= ltv_180'],
+            1,
+        )
+    ]
+)
 def test_queries(df, q, exp_status):
     assert qa.query_test(df, q) == exp_status
 
 
-def test_df_test_nullable(df=pd.DataFrame({1: [np.nan, 4], 2: [1, 2]})):
-    assert qa.df_test(df, nullable_cols=[1], strict=False) == 0
+def test_df_test_nullable(sample_data):
+    assert qa.df_test(sample_data, nullable_cols=[1], strict=False) == 0
 
 
-def test_df_test_strict(df=pd.DataFrame({1: [np.nan, 4], 2: [1, 2]})):
+def test_df_test_strict(sample_data):
     with pytest.raises(ValueError):
-        qa.df_test(df, strict=True)
-    assert qa.df_test(df, strict=False) == 1
+        qa.df_test(sample_data, strict=True)
+    assert qa.df_test(sample_data, strict=False) == 1
