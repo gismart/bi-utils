c659e9fe9ba7d8751d452def71974548690cb2b0
maxim-lisovsky-gismart <maxim.lisovsky@gismart.com>
2023-03-10 11:04:48 +0100

    add parquet aws.db tests

--- tests/aws/test_db.py
+++ tests/aws/test_db.py
@@ -14,47 +14,77 @@ def test_delete_wo_conditions():
         db.delete(table, schema=schema)
 
 
-def test_upload_download_delete():
+@pytest.mark.parametrize(
+    "file_format",
+    ["csv", "parquet"],
+)
+def test_upload_download_delete(file_format):
     version = 1
     db.delete(table, schema=schema, version=version)
+    timestamp = pd.Timestamp.now()
     data = pd.DataFrame(
         {
             "text": ["hello", "bye"],
             "predict_dt": ["2020-01-01", dt.date.today()],
             "version": [version, version],
+            "load_dttm": timestamp,
         }
     )
     data.predict_dt = pd.to_datetime(data.predict_dt)
-    db.upload_data(data, "/tmp/data.csv", schema=schema, table=table)
-    query = f"SELECT * FROM {schema}.{table} WHERE version = {version}"
-    downloaded_data = db.download_data(query, parse_dates=["predict_dt"], dtype={"version": "int"})
-    downloaded_data.pop("load_dttm")
+    db.upload_data(data, f"/tmp/data.{file_format}", schema=schema, table=table)
+    query = f"""
+        SELECT text, predict_dt, version, load_dttm
+        FROM {schema}.{table}
+        WHERE version = {version}
+        ORDER BY predict_dt
+    """
+    downloaded_data = db.download_data(
+        query,
+        file_format=file_format,
+        parse_dates=["predict_dt", "load_dttm"],
+        dtype={"version": "int"},
+    )
     assert downloaded_data.equals(data)
     db.delete(table, schema=schema, version=version)
     downloaded_data = db.download_data(query, parse_dates=["predict_dt"])
     assert downloaded_data.empty
 
 
-def test_upload_update_download():
+@pytest.mark.parametrize(
+    "file_format",
+    ["csv", "parquet"],
+)
+def test_upload_update_download(file_format):
     version = 1
     new_version = 2
     db.delete(table, schema=schema, version=version)
     db.delete(table, schema=schema, version=new_version)
+    timestamp = pd.Timestamp.now()
     data = pd.DataFrame(
         {
             "text": ["hello", "bye"],
             "predict_dt": ["2020-01-01", dt.date.today()],
             "version": [version, version],
+            "load_dttm": timestamp,
         }
     )
     data.predict_dt = pd.to_datetime(data.predict_dt)
-    db.upload_data(data, "/tmp/data.csv", schema=schema, table=table)
+    db.upload_data(data, f"/tmp/data.{file_format}", schema=schema, table=table)
     params_set = {"version": new_version}
     params_where = {"text": "bye"}
     db.update(table, schema=schema, params_set=params_set, params_where=params_where)
-    query = f"SELECT * FROM {schema}.{table} WHERE version IN ({version}, {new_version})"
-    downloaded_data = db.download_data(query, parse_dates=["predict_dt"], dtype={"version": "int"})
-    downloaded_data.pop("load_dttm")
+    query = f"""
+        SELECT text, predict_dt, version, load_dttm
+        FROM {schema}.{table}
+        WHERE version IN ({version}, {new_version})
+        ORDER BY predict_dt
+    """
+    downloaded_data = db.download_data(
+        query,
+        file_format=file_format,
+        parse_dates=["predict_dt", "load_dttm"],
+        dtype={"version": "int"},
+    )
     assert not downloaded_data.equals(data)
     version2_rows = (downloaded_data["version"] == new_version).sum()
     assert version2_rows == 1
@@ -63,4 +93,4 @@ def test_upload_update_download():
 
 def test_get_columns():
     db_columns = db.get_columns(table=table, schema=schema)
-    assert db_columns == ["text", "predict_dt", "version", "load_dttm"]
+    assert db_columns == ["text", "predict_dt", "load_dttm", "version"]
